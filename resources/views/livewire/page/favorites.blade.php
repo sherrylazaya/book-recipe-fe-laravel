08cd7168c84bd57a5314c93c96267490a3413ccb
alfhz <alfh.zachra@gmail.com>
2024-04-01 07:25:11 +0700

    upg: fav-indexchanges

--- resources/views/livewire/page/favorites.blade.php
+++ resources/views/livewire/page/favorites.blade.php
@@ -41,8 +41,8 @@
 
                 <div class="mx-md-2">
                     <div class="d-md-flex align-items-center justify-content-between mx-auto mt-4">
-                        <livewire:reusable.index-limitter> 
-                            <livewire:reusable.pagination-favorite :totalData="$recipes['total']" :key="$indexChanges">
+                        <livewire:reusable.index-limitter :key="$indexChanges"> 
+                            <livewire:reusable.pagination-favorite :totalData="$recipes['total']">
                     </div>
                 </div>
         </div>
